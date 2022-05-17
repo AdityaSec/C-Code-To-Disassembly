d41c02c57cb5f0d35a87b07e871968ce528e18f7
D3v1LaL <40142550+AdityaSec@users.noreply.github.com>
2022-05-17 20:43:21 +0530

    Add files via upload

---
+++ 7-array.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main(){
+	int myNumbers[] = {25, 50, 75, 100};
+	printf("%d", myNumbers[0]);
+}
---
+++ 8-string.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main () {
+   char msg[6] = "Hello";
+   printf("Output: %s\n", msg );
+   return 0;
+}
